9b4a45c56f6fdc931844d6363d776555d0b54bc0
Faust VIII <joh.faustviii@gmail.com>
2024-03-27 14:20:57 +0100

    Add missing constructor in ChannelController

--- chat/src/main/java/org/faust/chat/ChannelController.java
+++ chat/src/main/java/org/faust/chat/ChannelController.java
@@ -1,9 +1,6 @@
 package org.faust.chat;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,8 +10,12 @@ public class ChannelController {
 
     private final ChannelService channelService;
 
+    public ChannelController(ChannelService channelService) {
+        this.channelService = channelService;
+    }
+
     @PostMapping
-    public void addChannel(String name) {
+    public void addChannel(@RequestBody String name) {
         channelService.addChannel(name);
     }
 

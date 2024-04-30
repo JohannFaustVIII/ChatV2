d4c5a29ffcc6349413deaa3391aca40fae56f703
Faust VIII <joh.faustviii@gmail.com>
2024-04-30 14:44:15 +0200

    Allow OPTIONS requests

--- chat/src/main/java/org/faust/chat/config/WebConfig.java
+++ chat/src/main/java/org/faust/chat/config/WebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -50,9 +51,12 @@ public class WebConfig implements WebFluxConfigurer {
     @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
         return http
+                .cors(Customizer.withDefaults())
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(auth -> {
-                    auth.anyExchange().hasRole("chat_access");
+                    auth
+                            .pathMatchers(HttpMethod.OPTIONS, "**").permitAll()
+                            .anyExchange().hasRole("chat_access");
                 })
                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(jwt -> jwt
                         .jwtAuthenticationConverter(grantedAuthoritiesExtractor())))

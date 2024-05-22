d0a077918465c34bdfc5195bd5785453789e72f2
Faust VIII <joh.faustviii@gmail.com>
2024-05-22 15:19:31 +0200

    Add Spring dependencies for JPA and PostgreSQL

--- chat/build.gradle.kts
+++ chat/build.gradle.kts
@@ -12,7 +12,9 @@ repositories {
 }
 
 dependencies {
+    implementation("org.postgresql:postgresql")
     implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     testImplementation("org.springframework.boot:sspring-boot-starter-webflux-test")

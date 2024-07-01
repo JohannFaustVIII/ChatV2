e6d3d21b67a8dbbc0e698912cf261914270d8b52
Faust VIII <joh.faustviii@gmail.com>
2024-07-01 13:59:09 +0200

    WIP Listen to session destroyed

--- chat/src/main/java/org/faust/chat/user/SessionObserver.java
+++ chat/src/main/java/org/faust/chat/user/SessionObserver.java
@@ -1,6 +1,15 @@
 package org.faust.chat.user;
 
-public class StatusAspect {
+import org.springframework.context.event.EventListener;
+import org.springframework.security.web.session.HttpSessionDestroyedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SessionObserver{
+    @EventListener
+    public void onApplicationEvent(HttpSessionDestroyedEvent event) {
+        System.out.println("Session: " + event.getId() + " has ended. ---");
+    }
     // TODO: idea, update user status by monitoring hitting endpoints
     // adv: less work for frontend to send updates
     // dis: can get more problematic when user sends custom requests, can keep online forever

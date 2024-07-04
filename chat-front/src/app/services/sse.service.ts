40a3aed96b882382174d0124d3e72b5ea160c8c3
Faust VIII <joh.faustviii@gmail.com>
2024-07-04 14:57:01 +0200

    WIP Use HttpClient to get SSE

--- chat-front/src/app/services/sse.service.ts
+++ chat-front/src/app/services/sse.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ApiHttpService } from './api-http.service';
 import { Listener } from '../models/listener';
+import { HttpDownloadProgressEvent, HttpEventType } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,18 @@ export class SseService {
   listeners : {[id : string] : any} = {}
 
   constructor(private api : ApiHttpService) {
-    this.api.getStream<string>('/events').subscribe(data => {
-      console.log(data)
-      var key = typeof data === 'string' ? data : '';
-      if (key in this.listeners) {
-        this.listeners[key].notify();
-      }
+    var previous = ''; // to think: is it safe?
+    this.api.getStream<Object>('/events').subscribe(data => {
+        if (data.type === HttpEventType.DownloadProgress) {
+          var progress = data as HttpDownloadProgressEvent;
+          var toAnalyze = progress.partialText?.substring(previous.length);
+          previous = typeof progress.partialText === 'string' ? progress.partialText : '';
+          console.log(toAnalyze);
+          var key = typeof data === 'string' ? data : '';
+          if (key in this.listeners) {
+            this.listeners[key].notify();
+          }
+        }
     });
   }
 

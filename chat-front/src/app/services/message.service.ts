741a5dfb677e40775819a75fc755b573d91e7c7c
Faust VIII <joh.faustviii@gmail.com>
2024-07-16 13:53:22 +0200

    Parse message to correct interface

--- chat-front/src/app/services/message.service.ts
+++ chat-front/src/app/services/message.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ApiHttpService } from './api-http.service';
 import { Message } from '../models/message.model';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,17 +11,27 @@ export class MessageService {
   constructor(private api : ApiHttpService) { }
 
   getMessages(channelId : string) {
-    return this.api.get<Array<Message>>('/chat/' + channelId + '?limit=20');
+    return this.api.get<Array<Message>>('/chat/' + channelId + '?limit=20').pipe(
+      map((data) => this.mapToMessage(data))
+    );
   }
 
   getOlderMessages(channelId: string, firstMessageId: any) {
-    return this.api.get<Array<Message>>('/chat/' + channelId + '?limit=20&before=' + firstMessageId);
+    return this.api.get<Array<Message>>('/chat/' + channelId + '?limit=20&before=' + firstMessageId).pipe(
+      map((data) => this.mapToMessage(data))
+    );;
   }
   getMessagesAfter(channelId: string, lastMessageId: any) {
-    return this.api.get<Array<Message>>('/chat/' + channelId + '?limit=20&after=' + lastMessageId);
+    return this.api.get<Array<Message>>('/chat/' + channelId + '?limit=20&after=' + lastMessageId).pipe(
+      map((data) => this.mapToMessage(data))
+    );;
   }
 
   addMessage(channelName : string, message : any) {
     this.api.post('/chat/' + channelName, message);
+  }
+
+  private mapToMessage(data : Array<any>) {
+    return data.map((m) => m as Message);
   }
 }

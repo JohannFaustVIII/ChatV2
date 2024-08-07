e023b98eea2d860246d48612553a0fa8f9e76a62
Faust VIII <joh.faustviii@gmail.com>
2024-08-07 13:45:25 +0200

    Add tests for KeycloakService

---
+++ chat/src/main/java/org/faust/chat/exception/UserUnknownException.java
@@ -0,0 +1,8 @@
+package org.faust.chat.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "Requested user not found.", value = HttpStatus.NOT_FOUND)
+public final class UserUnknownException extends RuntimeException {
+}

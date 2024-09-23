d77e01fbe98d066c6861bf9113721797aab19856
Faust VIII <joh.faustviii@gmail.com>
2024-09-23 14:15:54 +0200

    Add idea for KeycloakRepository tests

---
+++ chat/src/test/java/org/faust/chat/keycloak/KeycloakRepositoryTest.java
@@ -0,0 +1,44 @@
+package org.faust.chat.keycloak;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KeycloakRepositoryTest {
+
+    // TODO: what does it require?
+    // repository is read only, so requires a testcontainer with set up users
+    // the best would be to set up empty container
+    // and put realms and users
+
+    @Test
+    public void whenNoUsersThenEmptyCollectionReturned() {
+
+    }
+
+    @Test
+    public void whenUsersExistThenAllReturned() {
+
+    }
+
+    @Test
+    public void whenUserExistsThenDetailsReturned() {
+
+    }
+
+    @Test
+    public void whenUserNotExistsThenNull() {
+
+    }
+
+    @Test
+    public void whenUserExistsThenTrue() {
+
+    }
+
+    @Test
+    public void whenUserNotExistsThenFalse() {
+
+    }
+
+}

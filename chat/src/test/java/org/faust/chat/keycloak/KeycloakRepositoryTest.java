bb8a98f6cf6fc4e5451f3dfe79e6db17c3dd7957
Faust VIII <joh.faustviii@gmail.com>
2024-09-24 14:26:10 +0200

    WIP: Set up Keycloak container for tests

--- chat/src/test/java/org/faust/chat/keycloak/KeycloakRepositoryTest.java
+++ chat/src/test/java/org/faust/chat/keycloak/KeycloakRepositoryTest.java
@@ -1,6 +1,23 @@
 package org.faust.chat.keycloak;
 
+import jakarta.ws.rs.core.Response;
+import org.faust.chat.user.UserDetails;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RealmRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
+
+import java.time.Duration;
+import java.util.Collection;
+import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,6 +28,78 @@ class KeycloakRepositoryTest {
     // the best would be to set up empty container
     // and put realms and users
 
+    private static GenericContainer<?> keycloakContainer;
+    private static Keycloak keycloak;
+
+    @BeforeAll
+    public static void setUp() {
+        keycloakContainer = setUpKeycloakContainer();
+        keycloak = setUpKeycloak();
+    }
+
+    private static GenericContainer<?> setUpKeycloakContainer() {
+        GenericContainer<?> container = new GenericContainer<>(DockerImageName.parse("quay.io/keycloak/keycloak"))
+                .withExposedPorts(8080)
+                .withEnv("KEYCLOAK_ADMIN", "admin")
+                .withEnv("KEYCLOAK_ADMIN_PASSWORD", "admin")
+                .waitingFor(Wait.forHttp("/realms/master").forStatusCode(200).withStartupTimeout(Duration.ofMinutes(5)))
+                .withCommand("start-dev");
+
+        container.start();
+
+        return container;
+    }
+
+    private static Keycloak setUpKeycloak() {
+        Keycloak k = KeycloakBuilder.builder()
+                .serverUrl("http://" + keycloakContainer.getHost() + ":" + keycloakContainer.getFirstMappedPort() + "/auth")
+                .realm("master")
+                .clientId("admin-cli")
+                .username("admin")
+                .password("admin")
+                .build();
+
+        RealmRepresentation newRealm = new RealmRepresentation();
+        newRealm.setRealm("test-realm");
+        newRealm.setEnabled(true);
+
+        k.realms().create(newRealm);
+
+        Keycloak result = KeycloakBuilder.builder()
+                .serverUrl("http://" + keycloakContainer.getHost() + ":" + keycloakContainer.getFirstMappedPort() + "/auth")
+                .realm("test-realm")
+                .clientId("admin-cli")
+                .username("admin")
+                .password("admin")
+                .build();
+
+        addUserToRealm(k, "User1");
+
+        return result;
+    }
+
+    private static void addUserToRealm(Keycloak keycloak, String username) {
+        UserRepresentation user = new UserRepresentation();
+        user.setUsername("username");
+        user.setFirstName("New");
+        user.setLastName("User");
+        user.setEmail(username + "@example.com");
+        user.setEnabled(true);
+
+        CredentialRepresentation password = new CredentialRepresentation();
+        password.setTemporary(false);
+        password.setType(CredentialRepresentation.PASSWORD);
+        password.setValue("user-password");
+
+        user.setCredentials(Collections.singletonList(password));
+
+        Response response = keycloak.realm("test-realm")
+                .users()
+                .create(user);
+
+        response.close();
+    }
+
     @Test
     public void whenNoUsersThenEmptyCollectionReturned() {
 
@@ -18,7 +107,13 @@ class KeycloakRepositoryTest {
 
     @Test
     public void whenUsersExistThenAllReturned() {
-
+        // given
+        KeycloakRepository testedRepository = new KeycloakRepository(keycloak);
+        // when
+        Collection<UserDetails> result = testedRepository.getUsers();
+        // then
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals("User1", result.iterator().next().name());
     }
 
     @Test
@@ -39,6 +134,13 @@ class KeycloakRepositoryTest {
     @Test
     public void whenUserNotExistsThenFalse() {
 
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        if (keycloakContainer != null) {
+            keycloakContainer.stop();
+        }
     }
 
 }

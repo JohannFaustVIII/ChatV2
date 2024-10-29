dcc2bbc6fec12d7cd7b94607349293aeb1667024
Faust VIII <joh.faustviii@gmail.com>
2024-10-29 15:01:22 +0100

    Add gateway module

---
+++ chat/gateway/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.2.4"
+    id("io.spring.dependency-management") version "1.1.4"
+
+}
+
+group = "org.faust"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.3")
+    }
+}

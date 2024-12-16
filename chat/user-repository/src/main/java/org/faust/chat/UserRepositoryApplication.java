09a6f91d53c67caa89650e8284413a0655724495
Faust VIII <joh.faustviii@gmail.com>
2024-12-16 15:07:08 +0100

    WIP: Add modules for user activity

---
+++ chat/user-repository/src/main/java/org/faust/chat/UserRepositoryApplication.java
@@ -0,0 +1,5 @@
+package org.faust.chat;
+
+public class UserRepositoryApplication {
+    // TODO: here implement getting activity messages from kafka + controller to return info about user state
+}
---
+++ chat/user-sse/src/main/java/org/faust/user/UserSSEApplication.java
@@ -0,0 +1,5 @@
+package org.faust.user;
+
+public class UserSSEApplication {
+    // TODO: here implement attaching to stream when active, and detaching, with sending data about it to repo
+}

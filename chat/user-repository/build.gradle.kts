1a28bfcf2a22542cc5a9011a9f46e50ad7737de1
Faust VIII <joh.faustviii@gmail.com>
2025-01-02 14:57:11 +0100

    WIP: Implement user repository

--- chat/user-repository/build.gradle.kts
+++ chat/user-repository/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.2.4"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "org.faust"
@@ -21,4 +23,10 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.3")
+    }
 }
--- chat/user-repository/src/main/java/org/faust/chat/UserRepositoryApplication.java
+++ chat/user-repository/src/main/java/org/faust/chat/UserRepositoryApplication.java
@@ -1,5 +1,15 @@
 package org.faust.chat;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableKafka
 public class UserRepositoryApplication {
-    // TODO: here implement getting activity messages from kafka + controller to return info about user state
+    public static void main(String[] args) {
+        SpringApplication.run(UserRepositoryApplication.class, args);
+    }
 }

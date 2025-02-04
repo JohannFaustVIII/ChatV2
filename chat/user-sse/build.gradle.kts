5f27ee21899e2e569e3a848224d987ebab0945cf
Faust VIII <joh.faustviii@gmail.com>
2025-02-04 15:04:00 +0100

    Build jars and images

--- chat/user-sse/build.gradle.kts
+++ chat/user-sse/build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 
 dependencies {
     implementation(project(":user-common"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.cloud:spring-cloud-starter-config")

944639e539d781028d29efc92b33886dcc393fba
Faust VIII <joh.faustviii@gmail.com>
2025-02-07 15:03:52 +0100

    WIP: Fix waiting for configuration server

--- chat/user-repository/src/main/java/org/faust/chat/UserRepositoryApplication.java
+++ chat/user-repository/src/main/java/org/faust/chat/UserRepositoryApplication.java
@@ -1,12 +1,15 @@
 package org.faust.chat;
 
+import org.faust.chat.external.KeycloakRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(clients = {KeycloakRepository.class})
 @EnableKafka
 public class UserRepositoryApplication {
     public static void main(String[] args) {

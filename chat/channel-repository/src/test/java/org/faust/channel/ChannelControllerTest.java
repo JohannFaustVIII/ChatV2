f9f03e2da4c47c6dff062af3638d2aac10777f9a
Faust VIII <joh.faustviii@gmail.com>
2025-04-09 13:10:59 +0200

    WIP: Test ChannelController of channel-repository

--- chat/channel-repository/src/test/java/org/faust/channel/ChannelControllerTest.java
+++ chat/channel-repository/src/test/java/org/faust/channel/ChannelControllerTest.java
@@ -1,6 +1,11 @@
 package org.faust.channel;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.faust.channel.command.CommandSerializer;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,13 +19,24 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import org.testcontainers.shaded.com.google.common.collect.ImmutableMap;
 import org.testcontainers.utility.DockerImageName;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.UncheckedIOException;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+
+// TODO: add postgres and liquibase files and setup
 
 @RunWith(SpringRunner.class)
 @Import(ChannelControllerTest.KafkaConfiguration.class)
@@ -40,17 +56,40 @@ class ChannelControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    public void givenChannelAddedWhenGettingChannelsThenReturned() {
+    public void givenChannelAddedWhenGettingChannelsThenReturned() throws Exception {
         // given
         // use kafka producer
+        KafkaProducer<String, Object> producer = new KafkaProducer<>(
+                ImmutableMap.of(
+                        ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers()
+                ),
+                new StringSerializer(),
+                new CommandSerializer()
+        );
         // when
+        ResultActions act = mockMvc.perform(MockMvcRequestBuilders.get("/channels"));
+        Collection<Channel> result = readContent(act.andReturn().getResponse().getContentAsByteArray());
 
         // then
+        Assertions.assertTrue(result.isEmpty());
     }
 
     @Test
     public void givenChannelAddedWhenCheckingExistenceThenTrue() {
 
+    }
+
+    private static Collection<Channel> readContent(byte[] data) {
+        ByteArrayInputStream bais = new ByteArrayInputStream(data);
+        try (ObjectInputStream ois = new ObjectInputStream(bais)) {
+            return (Collection<Channel>) ois.readObject();
+        }
+        catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+        catch (ClassNotFoundException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     @DynamicPropertySource
@@ -65,6 +104,8 @@ class ChannelControllerTest {
             configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
             configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringDeserializer.class);
             configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, org.faust.channel.command.CommandDeserializer.class);
+            configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-test");
+            configProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
             // more standard configuration
             return new DefaultKafkaConsumerFactory<>(configProps);
         }

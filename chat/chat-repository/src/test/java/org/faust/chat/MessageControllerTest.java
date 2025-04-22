42a721102a3a236a091c232e5699d04d2f505b1c
Faust VIII <joh.faustviii@gmail.com>
2025-04-22 15:13:21 +0200

    WIP: Add idea for E2E test for MessageController

---
+++ chat/chat-repository/src/test/java/org/faust/chat/MessageControllerTest.java
@@ -0,0 +1,9 @@
+package org.faust.chat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// TODO: there is endpoint to get all messages, and via kafka to edit data; so put data via kafka and then check existence? clean db between calls
+
+class MessageControllerTest {
+
+}
